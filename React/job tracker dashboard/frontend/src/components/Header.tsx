221ec6c06cf0e76037086e8595dfe972fa935598
Ilonczai András <ilonczai.andras16@gmail.com>
2025-04-02 10:44:53 +0200

    Job Tracker Dashboard
    Frontend refac

--- React/job tracker dashboard/frontend/src/components/Header.tsx
+++ React/job tracker dashboard/frontend/src/components/Header.tsx
@@ -4,36 +4,38 @@ import UserMenu from "./UserMenu";
 import { useEffect, useState } from "react";
 
 export const Header = () => {
-    const { isAuthenticated,getAccessTokenSilently } = useAuth0();
-    const [modalOpen, setModalOpen] = useState(false);
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [modalOpen, setModalOpen] = useState(false);
 
-    useEffect(() => {
-        if (!isAuthenticated) return;
-    
-        getAccessTokenSilently()
-            .then(token => console.log("Token: ", token))
-            .catch(err => console.error("Token hiba:", err));
-    
-    }, [isAuthenticated]);
+  useEffect(() => {
+    if (!isAuthenticated) return;
 
-    return (
-        <header className="flex justify-between items-center px-8 py-4 bg-gray-900 text-white shadow-md">
-            <h2 className="text-[1.3rem]">Job Tracker Dashboard</h2>
+    getAccessTokenSilently()
+      .then((token) => console.log("Token: ", token))
+      .catch((err) => console.error("Token hiba:", err));
+  }, [isAuthenticated, getAccessTokenSilently]);
 
-            <div className="flex items-center gap-4">
-                {isAuthenticated && (
-                    <button
-                        onClick={() => setModalOpen(true)}
-                        className="h-10 px-4 rounded-lg bg-white text-gray-800 shadow flex items-center hover:bg-gray-100 transition-colors"
-                    >
-                        + New Application
-                    </button>
-                )}
+  return (
+    <header className="flex justify-between items-center px-8 py-4 bg-gray-900 text-white shadow-md">
+      <h2 className="text-[1.3rem]">Job Tracker Dashboard</h2>
 
-                <UserMenu />
-            </div>
+      <div className="flex items-center gap-4">
+        {isAuthenticated && (
+          <button
+            onClick={() => setModalOpen(true)}
+            className="h-10 px-4 rounded-lg bg-white text-gray-800 shadow flex items-center hover:bg-gray-100 transition-colors"
+          >
+            + New Application
+          </button>
+        )}
 
-            <ApplicationModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
-        </header>
-    );
-}
+        <UserMenu />
+      </div>
+
+      <ApplicationModal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+      />
+    </header>
+  );
+};
